34f6aeed707f56f990bc241503b51ebbb388b33c
resueman14 <56014045+resueman14@users.noreply.github.com>
2021-08-01 23:43:12 +0500

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const Handlebars = require('handlebars')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const app = express()
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const homeRoutes = require('./routes/home')
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views','views')
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+
+async function start(){
+  try {
+    const PORT = process.env.PORT || 3000 
+    app.listen(PORT, () => {
+        console.log(`Server is running on port http://127.0.0.1:${PORT}`)
+    })
+  } catch (error) {
+    console.log(error) 
+  }
+
+}
+start()
+
