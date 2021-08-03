796939c8c4ccfc32a7f2ad15294df4d3be2c8ace
resueman14 <56014045+resueman14@users.noreply.github.com>
2021-08-03 23:40:25 +0500

    upload files

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,30 @@
 const {Router} = require('express')
 const router = Router()
-
 router.get('/', (req,res)=>{
   res.status(200)
   res.render('index',{title:'Главная', isHome: true})
+})
+
+router.get('/upload', (req,res)=>{
+  res.status(200)
+  res.render('index',{title:'Главная', isHome: true})
+})
+
+router.post('/upload', (req,res)=>{
+  if(req.files){
+    const file = req.files.upfile
+    const fileName = req.files.upfile.name
+    file.mv(`${__dirname}/../uploads/${fileName}`,err=>{
+      if(err){
+        console.log(err)
+        res.end('Error')
+      } else {
+        res.end('Uploaded')
+      }
+    })
+  }else{
+    res.end("There\'s no file")
+  }
 })
 
 module.exports = router
