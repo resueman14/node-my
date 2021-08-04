934e96745cf43ebcf2194db8d78e362f7490dc54
resueman14 <56014045+resueman14@users.noreply.github.com>
2021-08-05 00:19:35 +0500

    delete file from dir and db

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,14 @@
 const {Router} = require('express')
 const File = require('../models/file')
+const fs = require('fs')
 const router = Router()
-router.get('/', (req,res)=>{
+router.get('/', async (req,res)=>{
+  const filenames = await File.find()
   res.status(200)
-  res.render('index',{title:'Главная', isHome: true})
+  res.render('index',{title:'Главная', isHome: true, filenames})
 })
 
-router.get('/upload', (req,res)=>{
+router.get('/upload', async (req,res)=>{
   res.status(200)
   res.render('index',{title:'Главная', isHome: true})
 })
@@ -15,7 +17,7 @@ router.post('/upload',async (req,res)=>{
   if(req.files){
     const file = req.files.upfile
     const fileName = req.files.upfile.name
-    await file.mv(`${__dirname}/../uploads/${fileName}`,async err=>{
+    await file.mv(`${__dirname}/../public/uploads/${fileName}`,async err=>{
       if(err){
         console.log(err)
         res.end('Error')
@@ -24,11 +26,33 @@ router.post('/upload',async (req,res)=>{
           name: fileName
         })
         await file.save()
-        res.end('Uploaded')
+        res.redirect('/')
       }
     })
   }else{
     res.end("There\'s no file")
+  }
+})
+
+router.get('/get/:name', async (req,res)=>{
+  let filePath = `${__dirname}/../public/uploads/${req.params.name}`
+  //console.log( `${__dirname}/../public/uploads/${req.params.name}`)
+  res.download(filePath)
+})
+router.get('/remove/:name', async (req,res)=>{
+  let filePath = `${__dirname}/../public/uploads/${req.params.name}`
+  //console.log( `${__dirname}/../public/uploads/${req.params.name}`)
+  try{
+    await File.deleteOne({name:req.params.name})
+    await fs.unlink(filePath, err=>{
+      if(err){
+        console.log(err)
+      }else{
+        res.redirect('/')
+      }
+    })
+  } catch(err){
+    console.log(err)
   }
 })
 
